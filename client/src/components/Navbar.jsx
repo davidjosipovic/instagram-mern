31953b7bd9fb8d7fa0a666b84d60ab6189c6fa4f
David Josipović <davidjosipovic19@gmail.com>
2023-05-23 18:42:36 +0200

    ♡

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,9 +4,24 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div className="navigation">
-      Navigacija
+    <div className="Navbar">
+      <div className='UpNavbar'>
+        <div id='Logo'>Instagram</div>
+      <input id="Search" placeholder='Search'/>
+      <div id='Heart'>♡</div>
+      </div>
+      
+      <div className='DownNavbar'>
+      <div className='item'>♡</div>
+      <div className='item'>♡</div>
+      <div className='item'>♡</div>
+      <div className='item'>♡</div>
+      <div className='item'>♡</div>
+      <div className='item'>♡</div>
+      </div>
+
     </div>
+
   );
 };
 
